32ae8306afcca8fa2eb752a4fd82116309a67572
jmanjo123 <56357627+jmanjo123@users.noreply.github.com>
2020-05-01 16:38:31 -0400

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1109c6996c30746b81c353934838643f"
 
 # Google API Key
-g_key = "YOUR KEY HERE"
+g_key = "AIzaSyCkWVqJonJzlSzKxw1VaaKraNhD0YIk6E4"
